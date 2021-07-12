54575acda0d76dff9ad61ccc302511b865d31ec8
Justin <jdr1813@yahoo.com>
2021-07-12 18:55:52 -0500

    created html test page and a bunch of random functions

---
+++ JavaScript/functions.js
@@ -0,0 +1,46 @@
+// function capitalize(str) {
+//   return str[0].toUpperCase() + str.slice(1);
+// }
+
+// capitalize("tomato");
+
+// function sumArray(array) {
+//   let total = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     total += array[i];
+//   }
+//   return total;
+// }
+
+// sumArray([10, 10, 10, 10]);
+
+// function returnDay(num) {
+//   const daysOfWeek = {
+//     1: "Monday",
+//     2: "Tuesday",
+//     3: "Wednesday",
+//     4: "Thursday",
+//     5: "Friday",
+//     6: "Saturday",
+//     7: "Sunday",
+//   };
+
+//   if (num < 1 || num > 7) {
+//     return null;
+//   }
+//   return daysOfWeek[num];
+// }
+
+// function saveMe() {
+//   let heroes = ["spiderman", "batman", "superman"];
+//   function cryForHelp() {
+//     for (let hero of heroes) {
+//       console.log(`Please save me ${hero.toUpperCase()}`);
+//     }
+//   }
+//   cryForHelp();
+// }
+
+// saveMe();
+
+
