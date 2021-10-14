f0f65d98c80366efd54ffa87115af3b9089a8a6d
Justin <jdr1813@yahoo.com>
2021-10-14 17:29:12 -0500

    moved files around and renamed files to be more appropriate to what they are for.

---
+++ dadJokes/app.js
@@ -0,0 +1,17 @@
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  const newLI = document.createElement("li");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+};
+
+const getDadJoke = async () => {
+  const config = { headers: { Accept: "application/json" } };
+  const res = await axios.get("https://icanhazdadjoke.com", config);
+  return res.data.joke;
+};
+
+button.addEventListener("click", addNewJoke);
